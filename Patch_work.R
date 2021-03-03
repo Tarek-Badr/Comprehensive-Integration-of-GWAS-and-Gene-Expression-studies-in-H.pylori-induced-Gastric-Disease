8e78219977b94874223e8affb2742171766aa202
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2021-03-03 12:07:06 +0100

    Update Patch_work.R

--- Patch_work.R
+++ Patch_work.R
@@ -1,12 +1,15 @@
+rm(list = ls())
+
+setwd("C:/Users/ngs-adm/Desktop")
+
 devtools::install_github("thomasp85/patchwork")
-
-
 install.packages("patchwork")
 
+
 par(mfrow=c(3,1))
-P1 = BarPlot_Top20Family_GTDB
-P2 = BarPlot_Top20Genus_GTDB 
-P3 = BarPlot_Top20SPP_GTDB
+P1 = BarPlot_Top10_Pathways_GO
+P2 = BarPlot_Top10_Pathways_KEGG
+P3 = BarPlot_Top10_Pathways_all
 
 
 P1 + P2 + P3 + plot_layout(ncol = 1)
