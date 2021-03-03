fb642549f53d336e4fce0795b27df8f87fb7d18a
Tarek-Badr <43323534+Tarek-Badr@users.noreply.github.com>
2021-03-03 12:03:12 +0100

    Update Limma_DEGs_analysis.R

--- Limma_DEGs_analysis.R
+++ Limma_DEGs_analysis.R
@@ -17,6 +17,7 @@ library(pamr)
 library(Biobase)
 library(GEOquery)
 library(limma)
+library(genefilter)
 
 setwd("C:/Users/ngs-adm/Desktop/")
 
@@ -29,25 +30,26 @@ CellLinesData <- getGEOData(c("GSE39919", "GSE70394", "GSE74577", "GSE74492"))
 
 gset <- getGEO("GSE70394", GSEMatrix =TRUE, AnnotGPL=TRUE)
 if (length(gset) > 1) det <- grep("GPL6255", attr(gset, "names")) else det <- 1
-
 gsedata <- gset[[det]]
+exp_data <- exprs(gsedata)
+###########################################################################
 
 fvarLabels(gsedata) <- make.names(fvarLabels(gsedata))
 
 gsmid <- "010101"
-sml <- c()
-for (i in 1:nchar(gsmid)) { sml[i] <- substr(gsmid,i,i) }
+sml_GSE70394 <- c()
+for (i in 1:nchar(gsmid)) { sml_GSE70394[i] <- substr(gsmid,i,i) }
 
-exp_data <- exprs(gsedata)
 q_exp_data <- as.numeric(quantile(exp_data, c(0., 0.25, 0.5, 0.75, 0.99, 1.0), na.rm=T))
 LogC <- (q_exp_data[5] > 100) ||
   (q_exp_data[6]-q_exp_data[1] > 50 && q_exp_data[2] > 0) ||
   (q_exp_data[2] > 0 && q_exp_data[2] < 1 && q_exp_data[4] > 1 && q_exp_data[4] < 2)
 if (LogC) { exp_data[which(exp_data <= 0)] <- NaN
 exprs(gsedata) <- log2(exp_data) }
-sml <- paste("G", sml, sep="")   
 
-samplev <- as.factor(sml)
+sml_GSE70394 <- paste("G", sml_GSE70394, sep="")   
+
+samplev <- as.factor(sml_GSE70394) 
 
 gsedata$description <- samplev
 
@@ -55,7 +57,7 @@ design <- model.matrix(~ description + 0, gsedata)
 
 colnames(design) <- levels(samplev)
 
-datafit <- lmFit(gsedata, design)
+datafit <- lmFit(gsedata, design=model.matrix(~ description + 0, gsedata))
 
 cont <- makeContrasts(G1-G0, levels=design)
 
@@ -63,6 +65,7 @@ datafit_cont <- contrasts.fit(datafit, cont)
 
 datafit_cont <- eBayes(datafit_cont, 0.01)
 
+#DEGs_GSE70394 <- topTable(datafit_cont, adjust="fdr", coef=2)
 DEGs_GSE70394 <- topTable(datafit_cont, adjust="fdr")
 
 DEGs_GSE70394 <- subset(DEGs_GSE70394, select=c("ID","adj.P.Val","P.Value","t","B","logFC","Gene.symbol","Gene.title"))
